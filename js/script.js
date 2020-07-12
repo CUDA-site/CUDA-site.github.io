bed319cebedd28eeec44a6e1f02287239438feb7
CUDA-site <68185922+CUDA-site@users.noreply.github.com>
2020-07-12 13:59:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function home_function_button() {
+    alert('Вы уже на главной странице!');
+}
+
+function work_button_function() {
+    alert('Данная функция временно недоступна!');
+}
+
+function header_buttons_function() {
+    alert('Данный раздел еще не готов!');
+}
+
+function any_team_button_function() {
+    alert('Недоступно!');
+}
+
+function any_portfolio_button_function() {
+    alert('Данный раздел временно недоступен!');
+}
+
+function all_portfolio_button_function() {
+    alert('Вы уже в данном разделе!');
+}
+
+function load_more_button_function() {
+    alert('В данный момент более нет работ!');
+}
+
+function send_message_button_function () {
+    alert('В данный момент возможность отправки сообщений заморожена!');
+}
